d4cff22c41cde2ab70405a271ade364b0cbc72ab
Gauri Saksena <gaurisaksena612@gmail.com>
2025-08-24 23:50:43 +0530

    Initial commit

---
+++ generate_passward.py
@@ -0,0 +1,9 @@
+import random
+import string
+
+def generate_password(min_length, numbers=True, special_characters=True):
+    letters = string.ascii_letters
+    digits = string.digits
+    specisl = string.punctuation
+    
+generate_password(10)
