dee3905b9310fa7c16e74d5b5a415266fa763262
Gauri Saksena <gaurisaksena612@gmail.com>
2025-08-25 23:00:17 +0530

    day 2

--- generate_passward.py
+++ generate_passward.py
@@ -5,5 +5,11 @@ def generate_password(min_length, numbers=True, special_characters=True):
     letters = string.ascii_letters
     digits = string.digits
     specisl = string.punctuation
+
+    characters= letters
+    if numbers:
+        characters += digits
+    if special_characters:
+        characters += special
     
 generate_password(10)
