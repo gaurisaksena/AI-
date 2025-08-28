cac47fb6156d72c8ec5707f1ef1aad3f9a1bb601
Gauri Saksena <gaurisaksena612@gmail.com>
2025-08-28 23:31:33 +0530

    day 4 part 2

--- generate_passward.py
+++ generate_passward.py
@@ -4,7 +4,7 @@ import string
 def generate_password(min_length, numbers=True, special_characters=True):
     letters = string.ascii_letters
     digits = string.digits
-    specisl = string.punctuation
+    special = string.punctuation
 
     characters= letters
     if numbers:
@@ -18,8 +18,26 @@ def generate_password(min_length, numbers=True, special_characters=True):
     has_special = False
 
     while not meets_criteria or len(pwd) < min_length:
+        new_char = random.choice(characters)
+        pwd += new_char
+
+        if new_char in digits:
+            has_number = True
+        elif new_char in special:
+                has_special = True
+
+        meets_criteria = True 
+        if numbers:
+             meets_criteria = has_number
+             if special_characters:
+                  meets_criteria = meets_criteria and has_special
+
+    return pwd
+
+        
         
 
 
     
-generate_password(10)
+pwd = generate_password(10)
+print(pwd)
