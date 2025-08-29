f76bd7042fbdfe7f94efb38ad17cf2eeda36ba43
Gauri Saksena <gaurisaksena612@gmail.com>
2025-08-29 23:33:17 +0530

    day5 part 3

--- pig.py
+++ pig.py
@@ -20,7 +20,10 @@ import random
 def roll():
     min_value = 1
     max_value = 6
-    roll = random.radint(min_value, max_value)
+    roll = random.randint(min_value, max_value)
 
     return roll
+
+value = roll()
+print(value)
 
